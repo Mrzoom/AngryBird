d91ac74d0b57b9fcc2342aa47c7892f56baa86ac
zoom <zoom@zoomtekiMac.local>
2014-08-22 17:42:07 -0700

    Initial Commit

---
+++ AngryBird/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  AngryBird
+//
+//  Created by zoom on 14-8-22.
+//  Copyright (c) 2014年 Jiance Tong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
